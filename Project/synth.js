e99ac5329f3889b54d29c223721ddad0121b795b
Zulino <giulianogaladini@gmail.com>
2023-11-28 19:27:06 +0100

    LFO e filtEnv risolti. No insieme

--- Project/synth.js
+++ Project/synth.js
@@ -7,35 +7,13 @@ var o_global = {
     vibrato : 0
 }
 
-var osc1_param = {
-    waveform : 'sine',
-    is_square : false,
-    duty_cycle : 50,
-}
-
-var osc1_param = {
-    waveform : 'sine',
-    is_square : false,
-    duty_cycle : 50,
-    octave : 0,
-    detune : 0 
-}
-
-var noise_cutoff = 20000;
-
-var mixer = {
-    g_o1 : 0,
-    g_o2 : 0,
-    g_noise : 0
-}
-
 var filter_param = {
-    cutoff: 20000,
-    resonance : 0,
+    cutoff: 200,
+    resonance : 7,
     // keyboard_tracking : 0,
     type : 'lowpass',
-    env_amount : 0,
-    LFO_amount : 0
+    env_amount : 1000,
+    LFO_amount : 1000,
 }
 
 var LFO = {
@@ -46,73 +24,89 @@ var LFO = {
 
 var adsr_mix = {
     is_ar : false,
-    attack : 0.01,
-    decay : 0.01,
+    attack : 0.5,
+    decay : 0.5,
     sustain: 0.5,
-    release : 0.1 
+    release : 2 
 }
 
 var adsr_filter = {
     is_ar : false,
-    attack : 0.01,
-    decay : 0.01,
+    attack : 1,
+    decay : 0.6,
     sustain: 0.5,
-    release : 0.1 
+    release : 1
 }
 
 
 
-function createAmpEnv(a, d, s, r) {                
-    return ampEnv = new Tone.AmplitudeEnvelope({
+function createAmpEnv(a, d, s, r) { 
+    var ampEnv = new Tone.AmplitudeEnvelope();
+    ampEnv.set({
     attack: a,
     decay: d,
     sustain: s,
     release: r,
-    }).connect(filter);     
+    })               
+    return ampEnv;     
 }
 
 function createOscillator(freq, type, modtype, harm, mod) {
-    return fmOsc = new Tone.FMOscillator({
-    frequency: freq,
-    type: type,
-    modulationType: modtype,
-    harmonicity: harm,
-    modulationIndex: mod,
-    }).connect(ampEnv);
+    var fmOsc = new Tone.FMOscillator();
+    fmOsc.set({
+        frequency: freq,
+        type: type,
+        modulationType: modtype,
+        harmonicity: harm,
+        modulationIndex: mod,
+    })
+    return fmOsc;
 }
 
 function createFilter(freq, Q, type){
-    return filter = new Tone.BiquadFilter({
+    var filter = new Tone.Filter()
+    filter.set({
         frequency : freq,
         Q : Q,
-        type : type
-    }).toDestination();
-        // filter.frequency = freq;
-        // filter.Q = Q
-        // filter.type = type
-        // return filter;
+        type : type,
+    })
+    return filter;
 }
 
 
 
-var filter = createFilter(filter_param.cutoff, filter_param.Q, filter_param.type);
-var ampEnv = createAmpEnv(adsr_filter.attack,adsr_filter.decay,adsr_filter.sustain,adsr_filter.release);
-// ampEnv.connect(filter)
-// filter.connect(c.destination);
-
-
-// var gain = c.createGain();
-
-// filter.connect(ampEnv);
-// gain.connect(c.destination);
-
-
-function createOscillator(freq, type, modtype, harm, mod) {
-    return fmOsc = new Tone.FMOscillator({
-    frequency: freq,
-    type: type,
-    modulationType: modtype,
-    harmonicity: harm,
-    modulationIndex: mod
-    }).connect(ampEnv);
+function createFilterEnv(a, d, s, r) {
+    return FilterEnv = new Tone.Envelope({
+    attack: a,
+    decay: d,
+    sustain: s,
+    release: r,
+    }); 
 }
+
+function createLFO(frequency, min, max) {
+    return LFO = new Tone.LFO({
+        frequency: frequency,
+        min: min,
+        max: max,
+    });
+}
+
+var filter = createFilter(filter_param.cutoff, filter_param.resonance, filter_param.type);
+var ampEnv = createAmpEnv(adsr_mix.attack,adsr_mix.decay,adsr_mix.sustain,adsr_mix.release);
+var oscillator = createOscillator(440, "sine", "triangle", 2.7, 1);
+
+
+var EnvFilterAmount = new Tone.Gain(filter_param.env_amount);
+var filterEnv = createFilterEnv(adsr_filter.attack,adsr_filter.decay,adsr_filter.sustain,adsr_filter.release);
+var LFO = createLFO(LFO.frequency, filter_param.cutoff, filter_param.LFO_amount);
+
+oscillator.chain(ampEnv, filter, Tone.Destination);
+filterEnv.chain(EnvFilterAmount, filter.frequency);
+LFO.connect(filter.frequency);
+
+LFO.start();
+oscillator.start();
+    
+
+
